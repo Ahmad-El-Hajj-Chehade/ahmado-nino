84fb092b85f260c83e2c596055bf36daeedf5f8e
Ahmad <ahmad.elhajjchehade@futurelog.com>
2025-02-07 19:41:42 +0100

    Fix build issues for Cloudflare deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
           ☰
         </div>
       <ul className={isOpen ? "show" : ""}>
-        <li><a href="/ahmado-nino">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
