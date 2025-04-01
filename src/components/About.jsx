b110adb73578778c6cc02204c531c8734562bb30
Ahmad-El-Hajj-Chehade <69591723+Ahmad-El-Hajj-Chehade@users.noreply.github.com>
2025-04-01 23:53:52 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = () => (
       <div className="about-text">
         <h3>Hey, I'm Ahmad</h3>
         <p>
-          My Name is Ahmad and I live in Dottikon Aargau. I'm a bachelor student and software developer. After the covid-lockdowns, me and my friends started to organize partys. In 2022 we created the partylabel dyoevents, which was the leading party label for 16+ events in Aarau and Basel. At such an event I met Nino and he became a very good friend of mine and part of my team. I was always interested in aiming for perfection in everything I do. Although I've always liked taking pictures with my phone, I picked up a professional camera to capture the emotions of my events. I learned a lot about photography and was very lucky to meet a lot of people who asked me to capture the emotion and story of their event. Contact us and let's capture the emotions of your event together!
+          My Name is Ahmad and I live in Dottikon Aargau. I'm a bachelor student and software developer. After the covid-lockdowns, me and my friends started to organize partys. In 2022 we created the partylabel dyoevents, which was the leading party label for 16+ events in Aarau and Basel. I was always interested in aiming for perfection in everything I do. Although I've always liked taking pictures with my phone, I picked up a professional camera to capture the emotions of my events. I learned a lot about photography and was very lucky to meet a lot of people who asked me to capture the emotion and story of their event. Let's connect and capture the emotions of your event together!
         </p>
       </div>
       <div className="about-image reverse">
