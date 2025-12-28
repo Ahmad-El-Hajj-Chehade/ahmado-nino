496b2f7726337970921f0699e10c51894bf355c8
Ahmad El Hajj Chehade <ahmad.ehc@outlook.com>
2025-12-28 17:18:16 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import PlazaAlbum from "./pages/PlazaAlbum";
-import UmeaAlbum from "./pages/UmeaAlbum";
-
-import MuriFasnachtAlbum from "./pages/MuriFasnachtAlbum";
 import "./App.css";
-import PTB13Album from "./pages/PTB13Album";
-import DisneyAlbum from "./pages/DisneyAlbum";
+
 
 
 const App = () => {
@@ -17,11 +12,6 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/plaza_25_01_25" element={<PlazaAlbum />} />
-        <Route path="/umea_31_12_24" element={<UmeaAlbum />} />
-        <Route path="/muri-fasnacht" element={<MuriFasnachtAlbum />} />
-          <Route path="/ptb13" element={<PTB13Album />} />
-          <Route path="/disneyparty" element={<DisneyAlbum />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,68 +1,95 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
 import "./Portfolio.css";
 
 const Portfolio = () => {
+  useEffect(() => {
+    const scripts = [
+      "https://www.tiktok.com/embed.js",
+      "https://www.instagram.com/embed.js"
+    ];
+
+    scripts.forEach(src => {
+      if (!document.querySelector(`script[src="${src}"]`)) {
+        const script = document.createElement("script");
+        script.src = src;
+        script.async = true;
+        document.body.appendChild(script);
+      }
+    });
+
+    if (window.instgrm) {
+      window.instgrm.Embeds.process(); 
+    }
+  }, []);
+
   return (
     <section id="portfolio" className="portfolio">
-      <div className="portfolio-title">
-      <h2>Portfolio</h2>
-      </div>
-      <div className="albums">
-          <Link to="/disneyparty" className="album">
-              <h3>Disneyparty by ipartypeople</h3>
-              <p>
-                  Nightlife | People
-              </p>
-              <img
-                  src="kanzlei_17_05_25/JPEG/K_Disney-175.jpg"
-                  alt="Disneyparty_Ahmad"
-              />
-
-          </Link>
-          <Link to="/ptb13" className="album">
-              <h3>13 Years PLAZE TO BE</h3>
-              <p>
-                  Nightlife | People
-              </p>
-              <img
-                  src="plaza_29_03_25/JPEG/PTB13_Ahmado-12.jpg"
-                  alt="PTB13_Ahmad"
-              />
-
-          </Link>
-      <Link to="/muri-fasnacht" className="album">
-        <h3>Muri Fasnacht</h3>
-          <p>
-            Nightlife | People
-          </p>
-          <img
-            src="muri_08_02_2025/JPEG/08_02_muri-02.jpg"
-            alt="Muri Fasnacht"
-          />
-         
-        </Link>
-
-        <Link to="/plaza_25_01_25" className="album">
-        <h3>Plaza Lemon</h3>
-          <p>
-            Nightlife | People
-          </p>
-          <img
-            src="plaza_25_01_25/JPEG/20250126-IMG_5260-2.jpg"
-            alt="Plaza 25 01 2025"
-          />
-         
-        </Link>
-        <Link to="/umea_31_12_24" className="album">
-        <h3>Umea Simon Miles</h3>
-          <p>Nightlife | DJ</p>
-          <img src="unite_31_12_24/JPEG/SWI-30.jpg" alt="Unite" />
+      <div className="portfolio-container">
+        <div className="portfolio-grid">
           
-        </Link>
+          {/* Reihe 1: Fokus liegt auf dem ersten Artist */}
+          <div className="grid-item item-wide">
+            <img src="noelHoller_241225-18.jpg" alt="Noel Holler" />
+          </div>
 
+          {/* Reihe 2: Mix aus Hochformaten */}
+          <div className="grid-item item-tall">
+            <img src="SM_KF200925-09.jpg" alt="Crowd" />
+          </div>
+          <div className="grid-item item-tall">
+            <img src="111025sueri-20.jpg" alt="DJ" />
+          </div>
+
+          {/* Reihe 3: Grosses Querformat für Atmosphäre */}
+          <div className="grid-item item-wide">
+            <img src="211125_kaufleuten-5.jpg" alt="Main Stage" />
+          </div>
+
+          {/* Reihe 4: Social Media Integration */}
+          <div className="grid-item item-tall video-container">
+            <blockquote 
+              className="instagram-media" 
+              data-instgrm-permalink="https://www.instagram.com/reel/DRSZ0mfjVwY/" 
+              data-instgrm-version="14"
+            >
+              <a href="https://www.instagram.com/reel/DRSZ0mfjVwY/"></a>
+            </blockquote>
+          </div>
+          <div className="grid-item item-tall video-container">
+            <blockquote 
+              className="instagram-media" 
+              data-instgrm-permalink="https://www.instagram.com/reel/DOZF4rWjflu" 
+              data-instgrm-version="14"
+            >
+              <a href="https://www.instagram.com/reel/DOZF4rWjflu"></a>
+            </blockquote>
+          </div>
+          {/* Reihe 5: Weitere Event-Highlights */}
+          <div className="grid-item item-wide">
+            <img src="DSC09246.jpg" alt="Main Stage Zoom" />
+          </div>
+
+           <div className="grid-item item-tall">
+            <img src="noelHoller_241225-17.jpg" alt="Noel Holler" />
+          </div>
+          <div className="grid-item item-tall">
+            <img src="311025_vollgas-10.jpg" alt="Vollgas Party 2" />
+          </div>
+
+          {/* Reihe 6: YouTube Abschluss */}
+          <div className="grid-item item-wide">
+            <iframe 
+              src="https://www.youtube.com/embed/UiqWvjiSdq4" 
+              title="YouTube 1" 
+              allowFullScreen
+            ></iframe>
+          </div>
+          
+        </div>
       </div>
     </section>
+    
+           
   );
 };
 
